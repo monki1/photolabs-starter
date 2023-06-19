37709ba6a4d03a821e9aea3e1a4ee1b6b8186113
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-19 08:45:25 -0400

    added TopNavigationBar in HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import PhotoList from "./components/PhotoList";
+import TopicList from "./components/TopicList";
+import HomeRoute from "./routes/HomeRoute";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => (
   <div className="App">
-    <PhotoList/>
+    {/*<PhotoList/>*/}
+    {/*  <TopicList/>*/}
+      <HomeRoute/>
   </div>
 )
 
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
 import '../styles/HomeRoute.scss';
+import TopNavigationBar from "../components/TopNavigationBar";
+import PhotoList from "../components/PhotoList";
 
-const HomeRoute = () => 
-  <div className="home-route">
-    {/* Insert React */}
-  </div>
+const HomeRoute = () => {
 
+
+  return (
+      <div className='home-route'>
+        <TopNavigationBar  />
+
+        <PhotoList  />
+      </div>
+  );
+};
 export default HomeRoute;
