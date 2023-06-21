9fc0724218ad420bf826362ca1ea72cf29c0fd23
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-21 15:52:31 -0400

    mock data + minor fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-import PhotoList from "./components/PhotoList";
-import TopicList from "./components/TopicList";
+
 import HomeRoute from "./routes/HomeRoute";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => (
   <div className="App">
-    {/*<PhotoList/>*/}
-    {/*  <TopicList/>*/}
       <HomeRoute/>
   </div>
 )
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import '../styles/TopicList.scss';
 import TopicListItem from "./TopicListItem";
+import PhotoFavButton from "./PhotoFavButton";
 
 const TopicList = (props=TopicList.defaultProps) => {
   const {topics} = props;
